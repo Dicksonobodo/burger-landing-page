3846b6689cfd92c186545ac28f3bdd1de916b788
dickson <obododickson7@gmail.com>
2024-09-08 07:13:08 -0700

    second commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.list');
+
+hamburger.addEventListener('click', () => {
+  navList.classList.toggle('nav-active');
+});
